18c6700e13f8c1315bbdb967e1e7167e6fd82f6c
IceStone16 <94969820+IceStone16@users.noreply.github.com>
2022-01-25 23:29:38 +0530

    Add files via upload

--- realshit2.py
+++ realshit2.py
@@ -1,28 +1,81 @@
+from tkinter import *
 import tkinter as TK
-from tkinter.ttk import Button, Entry
+from PIL import Image,ImageTk,ImageOps
+import tkinter.filedialog
+from tkinter.ttk import *
+bc=0
+def MainOpen():
+    global img, im, MLabel,RSiz
+    mainscreen.FName = TK.filedialog.askopenfilename( title="Open Your Image" ,filetypes=(("jpg files", "*.jpg"), ("jpeg files", "*.jpeg"),("png files", "*.png")))
+    im = Image.open(mainscreen.FName)
+    RSiz = im.resize((600,600))
+    img = ImageTk.PhotoImage(RSiz)
+    Lab1 = Label(WorkingArea, image=img).place(x=1, y=0)
+def FLHor():
+    global RSiz
+    RSiz=RSiz.transpose(Image.FLIP_LEFT_RIGHT)
+    global hi
+    hi=ImageTk.PhotoImage(RSiz)
+    Lab1=Label(WorkingArea,image=hi).place(x=1,y=0)
+def FLVer():
+    global RSiz
+    RSiz=RSiz.transpose(Image.FLIP_TOP_BOTTOM)
+    global hi
+    hi=ImageTk.PhotoImage(RSiz)
+    Lab1=Label(WorkingArea,image=hi).place(x=1,y=0)
+def RTRit():
+    global RSiz
+    RSiz=RSiz.transpose(Image.ROTATE_270)
+    global hi
+    hi=ImageTk.PhotoImage(RSiz)
+    Lab1=Label(WorkingArea,image=hi).place(x=1,y=0)
+def RTLef():
+    global RSiz
+    RSiz=RSiz.transpose(Image.ROTATE_90)
+    global hi
+    hi=ImageTk.PhotoImage(RSiz)
+    Lab1=Label(WorkingArea,image=hi).place(x=1,y=0)
+def BlkNWht():
+    global bc
+    if bc%2==0:
+        bc=3
+        global Rsiz
+        global bcha
+        bcha=RSiz
+        bcha=bcha.convert("L")
+        global hi
+        hi=ImageTk.PhotoImage(bcha)
+        yechap=Label(WorkingArea,image=hi).place(x=1,y=0)
+    else:
+        hi=ImageTk.PhotoImage(RSiz)
+        yechap=Label(WorkingArea,image=hi).place(x=1,y=0)
+        bc=0
+def InvrtC():
+    global RSiz
+    pixels = RSiz.load()
+    for i in range(RSiz.size[0]):
+        for j in range(RSiz.size[1]):
+            x,y,z = pixels[i,j][0],pixels[i,j][1],pixels[i,j][2]
+            x,y,z = abs(x-255), abs(y-255), abs(z-255)
+            pixels[i,j] = (x,y,z)
+    global hi
+    hi=ImageTk.PhotoImage(RSiz)
+    Lab1=Label(WorkingArea,image=hi).place(x=1,y=1)
+def SAve():
+        global RSiz
+        RSiz= RSiz.save("save1.jpg")
 mainscreen=TK.Tk()
-WorkingArea=TK.Canvas(mainscreen,height=600,width=500,bg="black").grid(row=1,rowspan=12,column=0,columnspan=200)
-OFile=TK.Button(mainscreen,text="Open File",bg="black",fg="white").grid(row=0,column=0,sticky="W")
-SFile=TK.Button(mainscreen,text="Save File",bg="black",fg="white").grid(row=0,column=1,sticky="W")
-cropS=TK.Button(mainscreen,text="Select Crop",bg="black",fg="white").grid(row=1,column=201)
-cropA=TK.Button(mainscreen,text="Apply Corp",bg="black",fg="white").grid(row=1,column=203)
-flipH=TK.Button(mainscreen,text="Flip Horizontally",bg="black",fg="white").grid(row=2,column=201)
-flipV=TK.Button(mainscreen,text="Flip Vertically",bg="black",fg="white").grid(row=2,column=203)
-BandW=TK.Button(mainscreen,text="Black and White",bg="black",fg="white").grid(row=10,column=201)
-RotL=TK.Button(mainscreen,text="Rotate Left",bg="black",fg="white").grid(row=3,column=201)
-RotR=TK.Button(mainscreen,text="Rotate Right",bg="black",fg="white").grid(row=3,column=203)
-ScSatu=TK.Scale(mainscreen,from_=-100,to=100,orient=TK.HORIZONTAL).grid(row=4,column=201)
-SatuA=TK.Button(mainscreen,text="Apply Saturation",bg="black",fg="white").grid(row=4,column=203)
-ScShar=TK.Scale(mainscreen,from_=0,to=100,orient=TK.HORIZONTAL).grid(row=5,column=201)
-SharA=TK.Button(mainscreen,text="Apply Sharpness",bg="black",fg="white").grid(row=5,column=203)
-ScCont=TK.Scale(mainscreen,from_=-100,to=100,orient=TK.HORIZONTAL).grid(row=6,column=201)
-ContA=TK.Button(mainscreen,text="Apply Contrast",bg="black",fg="white").grid(row=6,column=203)
-ScExpo=TK.Scale(mainscreen,from_=-100,to=100,orient=TK.HORIZONTAL).grid(row=7,column=201)
-ExpoA=TK.Button(mainscreen,text="Apply Exposure",bg="black",fg="white").grid(row=7,column=203)
-HLBorder=TK.Button(mainscreen,text="Highlight Border",bg="black",fg="white").grid(row=8,column=202)
-TextE=TK.Entry(mainscreen,width=30).grid(row=9,column=201,columnspan=3)
-InTex=TK.Button(mainscreen,text="Insert Text",bg="black",fg="white").grid(row=10,column=202)
-FxPos=TK.Button(mainscreen,text="Fix Position",bg="black",fg="white").grid(row=10,column=203)
+mainscreen.title("Image Editor(beta version)")
+WorkingArea=TK.Canvas(mainscreen,height=600,width=600,bg="black").grid(row=1,rowspan=12,column=0,columnspan=200)
+OFile=TK.Button(mainscreen,text="Open File",bg="black",fg="white",command=MainOpen).grid(row=5,column=201,sticky="W")
+SFile=TK.Button(mainscreen,text="Save File",bg="black",fg="white",command=SAve).grid(row=5,column=202,sticky="W")
+cropS=TK.Button(mainscreen,text="Select Crop",bg="black",fg="white").grid(row=1,column=202)
+flipH=TK.Button(mainscreen,text="Flip Horizontally",bg="black",fg="white",command=FLHor).grid(row=2,column=201)
+flipV=TK.Button(mainscreen,text="Flip Vertically",bg="black",fg="white",command=FLVer).grid(row=2,column=203)
+BandW=TK.Button(mainscreen,text="Black and White",bg="black",fg="white",command=BlkNWht).grid(row=4,column=201)
+RotL=TK.Button(mainscreen,text="Rotate Left",bg="black",fg="white",command=RTLef).grid(row=3,column=201)
+RotR=TK.Button(mainscreen,text="Rotate Right",bg="black",fg="white",command=RTRit).grid(row=3,column=203)
+ContA=TK.Button(mainscreen,text="Invert Color",bg="black",fg="white",command=InvrtC).grid(row=4,column=203)
 mainscreen.mainloop()
 
 
